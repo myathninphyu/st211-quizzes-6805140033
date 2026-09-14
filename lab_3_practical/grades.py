28f2d06235999cde371b15b08fd9a4c9c0bd6269
myathninphyu <6805140033@siam.edu>
2026-09-14 13:38:35 +0700

    lab_practical_3: Adding the first program

---
+++ lab_3_practical/grades.py
@@ -0,0 +1,13 @@
+def letter_grade(score):
+    if score < 0 or score > 100:
+        raise ValueError("Score must be 0-100")
+
+    if score >= 80:
+        return 'A'
+
+    if score >= 70:
+        return 'B'
+
+    if score >= 60:
+        return 'C'
+    return 'F'
